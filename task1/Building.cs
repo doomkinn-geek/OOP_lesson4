dc20d075bc7ae4fa9cd36a2d917e72b5b71d275f
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-10-25 17:06:01 +0300

    fields added

---
+++ task1/Building.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1
+{
+    class Building
+    {
+        private uint _number;
+        private uint _buildingHeight;
+        private uint _floorsCount;
+        private uint _flatsNumber;
+        private uint _entrancesNumber;
+
+
+
+        public Building()
+        {
+            return;
+        }
+    }
+}
