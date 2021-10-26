0c376aa9f9155a51866be8419d490766561799cd
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-10-26 18:29:11 +0300

    properties

--- task1/Building.cs
+++ task1/Building.cs
@@ -9,10 +9,38 @@ namespace task1
         private uint _number;
         private uint _buildingHeight;
         private uint _floorsCount;
-        private uint _flatsNumber;
-        private uint _entrancesNumber;
+        private uint _flatsCount;
+        private uint _entrancesCount;
 
+        private static uint _lastBuildingNumber = 1;
 
+        public uint Number 
+        { 
+            get => _number;
+            set { _number = value; }
+        }
+
+        public uint BuildingHeight
+        {
+            get => _buildingHeight;
+            set { _buildingHeight = value; }
+        }
+
+        public uint FloorsCount
+        {
+            get => _floorsCount;
+            set { _floorsCount = value; }
+        }
+        public uint FlatsCount
+        {
+            get => _flatsCount;
+            set { _flatsCount = value; }
+        }
+        public uint EntrancesCount
+        {
+            get => _entrancesCount;
+            set { _entrancesCount = value; }
+        }
 
         public Building()
         {
